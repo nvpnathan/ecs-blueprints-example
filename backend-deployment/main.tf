ffca7eb6c05b86956d000cf8c07a7b4466350625
Nathan Ness <nathan79@gmail.com>
2022-07-07 09:58:46 -0700

    added dynamo var to main

--- backend-deployment/main.tf
+++ backend-deployment/main.tf
@@ -267,7 +267,8 @@ module "codebuild_app" {
   service_port           = local.app_server_port
   ecs_role               = "${local.name}-ecs"
   ecs_task_role          = module.ecs_service_app.task_role_arn
-
+  dynamodb_table_name    = module.assets_dynamodb_table.name
+  
   tags = local.tags
 }
 
